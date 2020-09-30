7cc7d630335b2b7406ab5b5f02c6b88a46f3e8a8
OliverTaiteJupp <38556804+gtfyds@users.noreply.github.com>
2020-09-30 11:25:14 +0100

    Added working function

--- AsciiConvertApp2/Program.cs
+++ AsciiConvertApp2/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace AsciiConvertApp2
+﻿using System;
+
+namespace asciiConvertApp2
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character");
+            string g = Console.ReadLine();
+            int asc = Convert.ToChar(g);
+            if (((65 <= asc) && (asc >= 90)) || ((95 <= asc) && (asc >= 120)))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
     }
-  }
 }
